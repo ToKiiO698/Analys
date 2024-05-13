b654057ed771731bf08fffbe4c0449d68e212fd6
ToKiiO698 <bilal.dira69@gmail.com>
2024-05-13 22:26:54 +0200

    affichage facture

--- commercial/a-facture.php
+++ commercial/a-facture.php
@@ -1488,25 +1488,37 @@
     </tr>
   </thead>
   <tbody>
-    <?php
-    require './bdd.php'; // Connexion à la base de données
+  <?php
+require './bdd.php'; // Connexion à la base de données
 
-    $stmt = $db->query('SELECT f.addr, f.tva, f.type_frais, f.id_facture, f.date_ajout, f.montant_ht, f.montant_ttc, z.name as nom_facture, z.id as file_id, f.num_fac, f.editeur FROM facture f INNER JOIN file z ON f.nom_facture = z.id WHERE f.etat_facture = 3');
+$stmt = $db->query('SELECT f.addr, f.date_ajout, f.num_fac, f.montant_ht, f.montant_ttc, 
+                   tva.taux as nom_tva, 
+                   type_frais.type as nom_type_frais, 
+                   u.nom as nom_editeur,
+                   z.name as nom_facture, z.id as file_id
+                   FROM facture f 
+                   INNER JOIN file z ON f.nom_facture = z.id 
+                   INNER JOIN user u ON f.editeur = u.id 
+                   INNER JOIN tva ON f.tva = tva.id
+                   INNER JOIN type_frais ON f.type_frais = type_frais.id_frais
+                   WHERE f.etat_facture = 3');
 
-    $factures = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    foreach ($factures as $facture): ?>
-      <tr>
+$factures = $stmt->fetchAll(PDO::FETCH_ASSOC);
+foreach ($factures as $facture): ?>
+    <tr>
         <td><?php echo $facture['addr']; ?></td>
         <td><?php echo $facture['num_fac']; ?></td>
         <td><?php echo $facture['montant_ht']; ?></td>
         <td><?php echo $facture['montant_ttc']; ?></td>
-        <td><?php echo $facture['tva']; ?></td>
+        <td><?php echo $facture['nom_tva']; ?></td>
         <td><?php echo $facture['date_ajout']; ?></td>
-        <td><?php echo $facture['type_frais']; ?></td>
-        <td><?php echo $facture['editeur']; ?></td>
+        <td><?php echo $facture['nom_type_frais']; ?></td>
+        <td><?php echo $facture['nom_editeur']; ?></td>
         <td><a href="recup_facture_file.php?editeur=<?php echo $facture['file_id']; ?>" target="_blank"><?php echo $facture['nom_facture']; ?></a></td>
-      </tr>
-    <?php endforeach; ?>
+    </tr>
+<?php endforeach; ?>
+
+
   </tbody>
 </table>
 
--- commercial/r-facture.php
+++ commercial/r-facture.php
@@ -1476,30 +1476,44 @@
       <th scope="col">TVA</th>
       <th scope="col">Date</th>
       <th scope="col">Type Frais</th>
+      <th scope="col">Editeur</th>
+      <th scope="col">Factures</th>
     </tr>
   </thead>
   <tbody>
-    <?php
-    // Connexion à la base de données
-    $db = new PDO('mysql:host=localhost;dbname=analys;charset=utf8mb4', 'root', '');
+  <?php
+require './bdd.php'; // Connexion à la base de données
 
-    // Requête pour récupérer toutes les données de la table facture
-    // Requête pour récupérer toutes les données de la table facture
-$stmt = $db->query('SELECT f.addr, f.tva, f.type_frais, f.id_facture, f.date_ajout, f.montant_ht, f.montant_ttc, f.justificatif, f.num_fac FROM facture f INNER JOIN etat_facture e ON f.etat_facture = e.id_etat WHERE f.etat_facture = 4');
-    $factures = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    foreach ($factures as $facture): ?>
-      <tr>
+$stmt = $db->query('SELECT f.addr, f.date_ajout, f.num_fac, f.montant_ht, f.montant_ttc, 
+                   tva.taux as nom_tva, 
+                   type_frais.type as nom_type_frais, 
+                   u.nom as nom_editeur,
+                   z.name as nom_facture, z.id as file_id
+                   FROM facture f 
+                   INNER JOIN file z ON f.nom_facture = z.id 
+                   INNER JOIN user u ON f.editeur = u.id 
+                   INNER JOIN tva ON f.tva = tva.id
+                   INNER JOIN type_frais ON f.type_frais = type_frais.id_frais
+                   WHERE f.etat_facture = 4');
+
+$factures = $stmt->fetchAll(PDO::FETCH_ASSOC);
+foreach ($factures as $facture): ?>
+    <tr>
         <td><?php echo $facture['addr']; ?></td>
         <td><?php echo $facture['num_fac']; ?></td>
         <td><?php echo $facture['montant_ht']; ?></td>
         <td><?php echo $facture['montant_ttc']; ?></td>
-        <td><?php echo $facture['tva']; ?></td>
+        <td><?php echo $facture['nom_tva']; ?></td>
         <td><?php echo $facture['date_ajout']; ?></td>
-        <td><?php echo $facture['type_frais']; ?></td>
-      </tr>
-    <?php endforeach; ?>
+        <td><?php echo $facture['nom_type_frais']; ?></td>
+        <td><?php echo $facture['nom_editeur']; ?></td>
+        <td><a href="recup_facture_file.php?editeur=<?php echo $facture['file_id']; ?>" target="_blank"><?php echo $facture['nom_facture']; ?></a></td>
+    </tr>
+<?php endforeach; ?>
   </tbody>
 </table>
+
+
 
 <!-- Inclure les fichiers JavaScript de DataTables -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
