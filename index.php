0577ddae7e2eaed3cb1367fa32cfae0fef7e578e
Antoine COBAUD <a.cob@outlook.fr>
2024-05-13 14:39:08 +0200

    css index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Commercial - Analys</title>
+    <title>Login Analys</title>
     <link
       href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
       rel="stylesheet"
@@ -12,6 +12,7 @@
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="assets/css/theme.min.css" />
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
       rel="stylesheet"
@@ -35,9 +36,16 @@
           />
         </div>
         <form action="index.php" method="post">
-        <input type="text" name="nom" placeholder="nom" required>
-        <input type="password" name="mdp" placeholder="mdp" required>
-        <button type="submit">Connexion</button>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Identifiant :</label>
+            <input type="password" class="form-control index-f" name="nom" required>
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label index-f">Mot de passe :</label>
+            <input type="password" class="form-control" name="mdp" required></br>
+          </div>
+
+          <button type="submit" class="btn btn-primary index-btn">Comnexion</button>
         <?php
         if (isset($_POST['nom']) && isset($_POST['mdp'])) {
             $_nom = $_POST['nom'];
@@ -67,9 +75,7 @@
         }   
         ?>
         </form>
-        <div id="formFooter">
-          <a class="underlineHover" href="#">Mot de passe oublié ?</a>
-        </div>
+        
       </div>
     </div>
   </body>
