f6d3a0eb281f6e09d4121ecb5429baecd3dc7daa
ToKiiO698 <bilal.dira69@gmail.com>
2024-05-14 00:05:23 +0200

    Paramètre affichage

--- admin.php
+++ admin.php
@@ -20,29 +20,29 @@
     <link
       rel="apple-touch-icon"
       sizes="180x180"
-      href="assets/img/favicons/apple-touch-icon.png"
+      href="assets/icon/analys.png"
     />
     <link
       rel="icon"
       type="image/png"
       sizes="32x32"
-      href="assets/img/favicons/favicon-32x32.png"
+      href="assets/icon/analys.png"
     />
     <link
       rel="icon"
       type="image/png"
       sizes="16x16"
-      href="assets/img/favicons/favicon-16x16.png"
+      href="assets/icon/analys.png"
     />
     <link
       rel="shortcut icon"
       type="image/x-icon"
-      href="assets/img/favicons/favicon.ico"
+      href="assets/icon/analys.png"
     />
     <link rel="manifest" href="assets/img/favicons/manifest.json" />
     <meta
       name="msapplication-TileImage"
-      content="assets/img/favicons/mstile-150x150.png"
+      content="assets/icon/analys.png"
     />
     <meta name="theme-color" content="#ffffff" />
     <script src="assets/js/config.js"></script>
