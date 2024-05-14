ee1b4e71fb887633cc4c29c2b14a03b156283533
ToKiiO <bilal.dira69@gmail.com>
2024-05-14 10:30:08 +0200

    prblm affichage jsp combien 2

--- commercial/n-facture.php
+++ commercial/n-facture.php
@@ -118,7 +118,7 @@
             <div class="d-flex flex-between-center">
               <button class="btn navbar-toggler-humburger-icon navbar-toggler me-1 me-sm-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDoubleTop" aria-controls="navbarDoubleTop" aria-expanded="false" aria-label="Toggle Navigation"><span class="navbar-toggle-icon"><span class="toggle-line"></span></span></button>
               <a class="navbar-brand me-1 me-sm-3" href="index.html">
-                <div class="d-flex align-items-center"><img class="me-2" src="assets/img/icons/spot-illustrations/falcon.png" alt="" width="40" /><span class="font-sans-serif text-primary">nalys</span></div> 
+                <div class="d-flex align-items-center"><img class="me-2" src="assets/img/icons/spot-illustrations/falcon.png" alt="" width="40" /><span class="font-sans-serif text-primary">falcon</span></div>
               </a>
               <ul class="navbar-nav align-items-center d-none d-lg-block">
                 <li class="nav-item">
@@ -1594,32 +1594,23 @@
         $file = $uniqueName . "." . $extension;
         $uploadPath = '../assets/facture/' . $file;
 
-        // Initialisation de la réponse sous forme de tableau
-$response = [];
+        // Déplacer le fichier téléchargé vers le répertoire des factures
+        if (move_uploaded_file($tmpName, $uploadPath)) {
+            // Enregistrement du nom du fichier dans la base de données
+            $req = $db->prepare('INSERT INTO file (name) VALUES (?)');
+            $req->execute([$file]);
 
-if ($fileSupported && $fileSizeOK) { // Assurez-vous que les variables $fileSupported et $fileSizeOK sont correctement définies avant cette condition
-    if (move_uploaded_file($tmpName, $uploadPath)) {
-        // Enregistrement du nom du fichier dans la base de données
-        $req = $db->prepare('INSERT INTO file (name) VALUES (?)');
-        $req->execute([$file]);
+            // Récupérer l'ID de la dernière facture insérée
+            $factureId = $db->lastInsertId();
 
-        // Récupérer l'ID de la dernière entrée insérée
-        $factureId = $db->lastInsertId();
-
-        // Stockage de la réponse réussie
-        $response = ['OK' => true, 'facture_id' => $factureId, 'file_path' => $uploadPath];
+            // Retourner le chemin du fichier de la facture
+            echo json_encode(['OK' => true, 'facture_id' => $factureId, 'file_path' => $uploadPath]); 
+        } else {
+            echo json_encode(['OK' => false, 'message' => "Erreur lors de l'enregistrement du fichier"]);
+        }
     } else {
-        // Stockage de la réponse en cas d'erreur d'enregistrement
-        $response = ['OK' => false, 'message' => "Erreur lors de l'enregistrement du fichier"];
+        echo json_encode(['OK' => false, 'message' => "Format de fichier non supporté ou taille de fichier trop grande"]);
     }
-} else {
-    // Stockage de la réponse en cas de format non supporté ou taille trop grande
-    $response = ['OK' => false, 'message' => "Format de fichier non supporté ou taille de fichier trop grande"];
-}
-
-// Affichage de la réponse en JSON
-echo json_encode($response);
-
 
                   $stmt = $db->prepare('INSERT INTO facture (etat_facture,date_ajout, montant_ht, montant_ttc, type_frais, editeur, addr, num_fac, tva, nom_facture) VALUES (:etat_facture, :dateV, :ht, :ttc, :type_frais, :editeur, :addr, :num_fac, :tva, :nom_facture)');
                   $stmt->bindParam(':etat_facture',$etat_facture );
